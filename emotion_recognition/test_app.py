844580a4c3805df1854bddc8c5666fe278b802a1
PismarovMikhail <mikepiamarov@gmail.com>
2023-09-08 00:29:30 +0300

    Create branch testing

--- emotion_recognition/test_app.py
+++ emotion_recognition/test_app.py
@@ -10,3 +10,35 @@ def test_read_app():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, dear friend"}
+    
+    
+def test_create_image_properties():
+    """_summary_
+    """
+    pass
+
+
+def test_predict_image():
+    """_summary_
+    """
+    pass
+
+
+def test_read_image():
+    """_summary_
+    """
+    pass
+
+
+def test_process_image():
+    """_summary_
+    """
+    pass
+
+
+
+
+
+    
+    
+
