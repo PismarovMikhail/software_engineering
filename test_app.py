2fc6d45a3dd61727299887f792aaccd68521b462
mike <mike@Ubuntu.myguest.virtualbox.org>
2023-09-16 01:45:44 +0300

    Edit app.py

--- test_app.py
+++ test_app.py
@@ -5,8 +5,6 @@ client = TestClient(app)
 
 
 def test_read_app():
-    """_summary_
-    """
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, dear friend!"}
