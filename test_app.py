b0f0405d0927686a70ca0e2cda8c58a2a8c8f64e
mike <mike@Ubuntu.myguest.virtualbox.org>
2023-09-15 06:01:48 +0300

    Check test_app.py

--- test_app.py
+++ test_app.py
@@ -12,10 +12,9 @@ def test_read_app():
     assert response.json() == {"message": "Hello, dear friend!"}
 
 
-"""
 def test_pay_attention():
     response = client.get("/attetion")
     assert response.status_code == 200
     assert response.json() == {"message": "See ResNet50 Class List on"
                                "deeplearning.cms.waikato.ac.nz"}
-"""
+
